b145dc410d2e44fe6aa844cd9419904fa40f66be
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-03-07 19:03:32 +0700

    membuat void input untuk memasukkan jumlah array dan menampilkan perintah untuk memasukkan data

--- 100_BubbleSort(20)/100_BubbleSort(20).cpp
+++ 100_BubbleSort(20)/100_BubbleSort(20).cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int a[10]; // jumlah kapasitas array
 int n; // jumlah data
+
+void input() {
+	while (true) {
+		cout << "Masukkan jumlah data: "; //output ke layar
+		cin >> n; //input dari user
+		if (n <= 10) { //jika n kurang dari sama dengan 10
+			break; //keluar dari loop
+		}
+		else {
+			cout << "Jumlah data tidak boleh lebih dari 10" << endl; //output ke layar
+		}
+	}
+
+	cout << endl; //output ke layar
+	cout << "=============================" << endl; //output ke layar
+	cout << "Masukkan data: " << endl; //output ke layar
+	cout << "=============================" << endl; //output ke layar
+
+	for (int i = 0; i < n; i++) { //looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << i + 1 << ": "; //output ke layar
+		cin >> a[i]; //input dari user
+	}
+}
