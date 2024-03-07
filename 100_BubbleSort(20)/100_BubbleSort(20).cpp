d2976f6f23f064e5b24cb68f2e331be4833611c8
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-03-07 19:04:36 +0700

    membuat void bubbleSortArray yang berfungsi untuk mengurutkan data dan menampiljkan pass yang dikerjakan beserta hasilnya

--- 100_BubbleSort(20)/100_BubbleSort(20).cpp
+++ 100_BubbleSort(20)/100_BubbleSort(20).cpp
@@ -26,3 +26,25 @@ void input() {
 		cin >> a[i]; //input dari user
 	}
 }
+
+void bubbleSortArray() {
+	int pass = 1; //inisialisasi pass dengan 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //looping dengan j dimulai dari 0 hingga n-1-pass
+			if (a[j] > a[j + 1]) { //jika a[j] lebih besar dari a[j+1]
+				int temp = a[j]; //simpan a[j] ke dalam variabel temp
+				a[j] = a[j + 1]; //a[j] diisi dengan a[j+1]
+				a[j + 1] = temp; //a[j+1] diisi dengan temp
+			}
+		}
+		pass = pass + 1; //pass ditambah 1
+
+		cout << "\nPass ke-" << pass - 1 << ": "; //output ke layar
+		for (int i = 0; i < n; i++) { //looping dengan i dimulai dari 0 hingga n-1
+			cout << a[i] << " "; //output ke layar
+		}
+		cout << endl; //output ke layar
+
+	} while (pass <= n - 1); //looping jika pass kurang dari sama dengan n-1
+}
